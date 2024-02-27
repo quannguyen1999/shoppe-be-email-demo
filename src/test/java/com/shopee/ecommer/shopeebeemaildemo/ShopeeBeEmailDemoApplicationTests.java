60df50cc5228f5e0a803f0715f40aea02f3697ff
quannguyen1999 <nguyendanganhquan99@gmail.com>
2024-02-27 13:14:27 +0700

    'init'

---
+++ src/test/java/com/shopee/ecommer/shopeebeemaildemo/ShopeeBeEmailDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopee.ecommer.shopeebeemaildemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopeeBeEmailDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
